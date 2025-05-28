ce52789461071300b9c4be1b5ed9ac2a40ec669e
Mikołaj Kokot <panmeek@proton.me>
2025-05-28 19:36:37 +0200

    Add universal operate function

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+let num1 = 0;
+let num2 = 0;
+let operator = "";
+
 function add(a, b) {
     return a + b;
 }
@@ -12,4 +16,21 @@ function multiply(a, b) {
 
 function divide(a, b) {
     return a / b;
+}
+
+function operate(num1, num2, operator) {
+    switch (operator) {
+        case "+":
+            return add(num1, num2);
+            break;
+        case "-":
+            return subtract(num1, num2);
+            break;
+        case "*":
+            return multiply(num1, num2);
+            break;
+        case "/":
+            return divide(num1, num2);
+            break;
+    }
 }
